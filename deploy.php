acb2307829f78e587745fe2fe3f938c9fe636844
Asper <asperwon@gmail.com>
2018-05-20 16:05:15 +0800

    replace with variables.

--- deploy.php
+++ deploy.php
@@ -3,10 +3,13 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
-// setting
-set('application', 'my_project');
-set('host', 'qa.local');
-set('repository', 'git@gitlab.local:asper/testci.git');
+// Project
+set('project_name', getenv('CI_PROJECT_NAME'));
+set('repository', getenv('CI_REPOSITORY_URL'));
+
+// host
+set('deploy_path', getenv('DEPLOY_PATH'));
+set('deploy_host', getenv('DEPLOY_SERVER'));
 
 // Shared files/dirs between deploys
 add('shared_files', []);
@@ -15,15 +18,19 @@ add('shared_dirs', []);
 // Writable dirs by web server
 add('writable_dirs', []);
 
-set('allow_anonymous_stats', false);
-set('writable_mode', 'chmod');
-
 
 // Hosts
-host(get('host'))
+host(get('deploy_host'))
     ->user('deployer')
     ->addSshOption('StrictHostKeyChecking', 'no')
-    ->set('deploy_path', '/var/www/{{application}}');
+    ->set('deploy_path', get('deploy_path').'/'.get('project_name'));
 
 // Tasks
-task('artisan:optimize', function () {});   // @overwrite
+task('artisan:optimize', function () {
+});   // @overwrite
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+before('deploy:symlink', 'artisan:migrate');
