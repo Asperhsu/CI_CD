795c8cbe25becaf4d4179af1cadddb4a6dfa6adb
Asper Hsu <asper@strategicsale.com>
2018-05-20 08:43:38 +0800

    first commit

---
+++ deploy.php
@@ -0,0 +1,29 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// setting
+set('application', 'my_project');
+set('host', 'qa.local');
+set('repository', 'git@gitlab.local:asper/testci.git');
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+set('allow_anonymous_stats', false);
+set('writable_mode', 'chmod');
+
+
+// Hosts
+host(get('host'))
+    ->user('deployer')
+    ->addSshOption('StrictHostKeyChecking', 'no')
+    ->set('deploy_path', '/var/www/{{application}}');
+
+// Tasks
+task('artisan:optimize', function () {});   // @overwrite
